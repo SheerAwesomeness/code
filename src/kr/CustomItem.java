c146aae17e28f58687de60b46ec55c8482f0e2fc
SheerAwesomeness <awesomeguyis@hotmail.co.uk>
2014-03-07 11:33:07 +0000

    ..
    ..

--- src/kr/CustomItem.java
+++ src/kr/CustomItem.java
@@ -3,4 +3,16 @@ package kr;
 
 public class CustomItem {
 
+
+
+  public void repeatPlayParticles(ParticleEffect pe, List<Location>, int times, int tickperiod){
+    Bukkit.getScheduler().runTaskLater(this, new Runnable(){
+      public void run(){
+        
+      }
+    }
+  }
+
+
+
 }
