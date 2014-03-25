b3866cf4b58c74144823e11532d7e0b9482219cb
SheerAwesomeness <awesomeguyis@hotmail.co.uk>
2014-03-25 09:33:34 +0000

    Update CustomItem.java
    Pre-Ability set.

--- src/kr/CustomItem.java
+++ src/kr/CustomItem.java
@@ -4,9 +4,28 @@ package kr;
 public class CustomItem {
 
 
+public ItemStack setAbility(ItemStack item, KPlayer kpl){
+  ItemStack is = item.clone();
+  ItemStack im = is.getItemMeta();
+  List<String> lore = im.getLore();
+  List<String> newlore = new ArrayList<String>();
+  if (kpl.hasAbility(im.getDisplayName())){
+   newlore.add("§8(§e1§8/§e1§8)");
+   newlore.add("§aLearnt.");
 
+  }else{
+      newlore.add("§8(§e0§8/§e1§8)");
+
+
+  }
+  newlore.add("§6Ability:");
+  newlore.addAll(lore);
+   
+   im.setLore(newlore);
+   is.setItemMeta(im);
+   return is;
  
-
+}
 
 
 }
